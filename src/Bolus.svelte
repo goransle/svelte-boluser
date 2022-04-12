6eeac0231fabb8ffee30288fa0b33d712eb48234
Gøran A. Slettemark <goransle@gmail.com>
2022-04-12 14:01:32 +0200

    added nice buttons

--- src/Bolus.svelte
+++ src/Bolus.svelte
@@ -15,7 +15,7 @@
         },
         body: JSON.stringify({
           insulin: IUs,
-          units: 'IU',
+          units: "IU",
           eventType: "Meal bolus",
           entered_by: "boluser",
         }),
@@ -29,6 +29,67 @@
 </script>
 
 <main>
-  <input type="number" bind:value={IUs} step={step} />
-  <input on:click={sendRequest} type="submit" />
+  <div id="input">
+    <button
+      on:click={() => {
+        IUs -= 0.5;
+      }}>-½</button
+    >
+    <button
+      on:click={() => {
+        IUs -= 1;
+      }}>-1</button
+    >
+    <input type="number" bind:value={IUs} {step} />
+    <button
+      on:click={() => {
+        IUs += 0.5;
+      }}>+½</button
+    >
+      <button
+      on:click={() => {
+        IUs += 1;
+      }}>+1</button
+    >
+  </div>
+  <div id="singles">
+    {#each [0.5, 1, 1.5, 2, 3] as value}
+      <button
+        on:click={() => {
+          IUs = value;
+        }}>{value}</button
+      >
+    {/each}
+  </div>
+  <input on:click={sendRequest} type="submit" value="Submit" />
 </main>
+
+<style>
+  main{
+    width: 100%;
+  }
+  div {
+    margin: 1em 0;
+  }
+  #singles {
+      display:flex;
+  }
+  #singles button {
+    width: 100px;
+    height: 80px;
+  }
+   #input {
+    display:flex;
+    align-items: center;
+    align-content: space-around;
+    width: 100%;
+    height: 8em;
+  }
+  #input button {
+    width: 12em;
+    height: 100%;
+  }
+    #input input {
+      width: 3em;
+  }
+</style>
