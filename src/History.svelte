63f5a9394ec38108e2483de53cc98686e7df97b0
Gøran A. Slettemark <goransle@gmail.com>
2022-04-13 08:17:38 +0200

    improved loading, added reload data button

--- src/History.svelte
+++ src/History.svelte
@@ -1,28 +1,40 @@
 <script lang="ts">
   import { API_URL } from "./stores";
   import { onMount } from "svelte";
+  import { Jumper } from 'svelte-loading-spinners'
 
   onMount(() => {
-    const interval = setInterval(getTreatments, 5000);
-    return () => clearInterval(interval);
+    getTreatments().then(()=>{
+      const interval = setInterval(getTreatments, 5000);
+      return () => clearInterval(interval);
+    })
   });
 
   async function getTreatments() {
+    isLoading = true;
     const req = new Request(
-      [$API_URL, "/api/v1/", "treatments?count=3&find[insulin][$gte]=0"].join(
-        ""
-      )
+      [$API_URL, "/api/v1/", "treatments?count=3&find[insulin][$gte]=0"]
+        .join("")
     );
     const response = await fetch(req);
     data = await response.json();
+    setTimeout(() => {
+      isLoading = false;
+    }, 1000);
   }
 
   let data;
+  let isLoading = true;
+
 </script>
 
 <main>
+  {#if !data && isLoading}
+    <Jumper></Jumper>
+  {/if}
   {#if data}
-    <table>
+  <div style="display:inline-flex;">
+    <table class:loading="{isLoading}">
       <thead>
         <tr>
           <th> Date </th>
@@ -40,5 +52,15 @@
         {/each}
       </tbody>
     </table>
+      <button on:click={getTreatments} alt="Reload data">🔄</button>
+      </div>
   {/if}
 </main>
+<style>
+  table.loading {
+    border: 1px dotted black;
+  }
+  th {
+    text-align: left;
+  }
+</style>
