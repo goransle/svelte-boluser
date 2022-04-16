02ebafaacc2e444765b9fe2f8e7b6fcd4ea34104
Gøran A. Slettemark <goransle@gmail.com>
2022-04-16 08:04:17 +0200

    minor tweaks

--- src/Bolus.svelte
+++ src/Bolus.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-import { select_value } from 'svelte/internal';
-
   import { API_KEY, API_URL } from "./stores";
 
-  const eventTypes = [
-    'Meal bolus',
-    'Correction',
-    'Other'
-  ];
+  const eventTypes = ["Meal bolus", "Correction bolus", "Other"];
 
   let IUs = 1;
   let insulin = "Novorapid";
@@ -40,21 +34,25 @@ import { select_value } from 'svelte/internal';
   <div id="input">
     <button
       on:click={() => {
-        IUs -= 0.5;
-      }}>-½</button
-    >
-    <button
-      on:click={() => {
         IUs -= 1;
-      }}>-1</button
-    >
-    <input type="number" bind:value={IUs} {step} />
-    <button
-      on:click={() => {
-        IUs += 0.5;
-      }}>+½</button
-    >
+      }}
+      >-1
+    </button>
+    {#if step === 0.5}
       <button
+        on:click={() => {
+          IUs -= 0.5;
+        }}>-½</button
+      >
+    {/if}
+    <input type="number" bind:value={IUs} {step} />
+    {#if step === 0.5}
+      <button
+        on:click={() => {
+          IUs += 0.5;
+        }}>+½</button>
+    {/if}
+    <button
       on:click={() => {
         IUs += 1;
       }}>+1</button
@@ -70,33 +68,37 @@ import { select_value } from 'svelte/internal';
     {/each}
   </div>
   <div>
-  <select on:change={(e)=>{
-    eventType = e.currentTarget.value;
-  }}>
-    {#each eventTypes as eventOption }
-      <option selected ="{eventOption === eventType}" value="{eventOption}">{eventOption}</option>
-    {/each}
-  </select>
+    <select
+      on:change={(e) => {
+        eventType = e.currentTarget.value;
+      }}
+    >
+      {#each eventTypes as eventOption}
+        <option selected={eventOption === eventType} value={eventOption}
+          >{eventOption}</option
+        >
+      {/each}
+    </select>
   </div>
   <input on:click={sendRequest} type="submit" value="Submit" />
 </main>
 
 <style>
-  main{
+  main {
     width: 100%;
   }
   div {
     margin: 1em 0;
   }
   #singles {
-      display:flex;
+    display: flex;
   }
   #singles button {
     width: 100px;
     height: 80px;
   }
-   #input {
-    display:flex;
+  #input {
+    display: flex;
     align-items: center;
     align-content: space-around;
     width: 100%;
@@ -106,7 +108,7 @@ import { select_value } from 'svelte/internal';
     width: 12em;
     height: 100%;
   }
-    #input input {
-      width: 3em;
+  #input input {
+    width: 3em;
   }
 </style>
